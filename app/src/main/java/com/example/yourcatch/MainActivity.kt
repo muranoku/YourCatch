787e4fc169cb6dd283a0145f65561bef04b537de
garagara <ayuyeaun@gmail.com>
2022-04-07 15:48:57 +0900

    add:画面構成

--- app/src/main/java/com/example/yourcatch/MainActivity.kt
+++ app/src/main/java/com/example/yourcatch/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.yourcatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //view取得
+        val btnAnime: Button = findViewById(R.id.btnAnime)
+        val btnMovie: Button = findViewById(R.id.btnMovie)
+        val btnBook: Button = findViewById(R.id.btnBook)
+        val btnComic: Button = findViewById(R.id.btnComic)
+
+        btnAnime.setOnClickListener {
+            val intent = Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
