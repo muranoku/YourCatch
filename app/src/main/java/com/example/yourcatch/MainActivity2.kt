2010b58ec7f561e1d8adf0e9533ada8ac33a48bd
garagara <ayuyeaun@gmail.com>
2022-04-10 02:35:30 +0900

    add:画面遷移追加

--- app/src/main/java/com/example/yourcatch/MainActivity2.kt
+++ app/src/main/java/com/example/yourcatch/MainActivity2.kt
@@ -1,11 +1,20 @@
 package com.example.yourcatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val anime1:ImageView = findViewById(R.id.anime1)
+
+        anime1.setOnClickListener {
+            val intent = Intent(this,MainActivity3::class.java)
+            startActivity(intent)
+        }
     }
 }
