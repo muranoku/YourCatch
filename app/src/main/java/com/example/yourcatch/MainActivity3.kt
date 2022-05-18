182aade44ee4ede707f1bc6d94dfdefe9575dad4
garagara <ayuyeaun@gmail.com>
2022-05-18 09:37:21 +0900

    add:firebase連携

--- app/src/main/java/com/example/yourcatch/MainActivity3.kt
+++ app/src/main/java/com/example/yourcatch/MainActivity3.kt
@@ -2,8 +2,17 @@ package com.example.yourcatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CancellationSignal
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity3 : AppCompatActivity() {
+
+    private lateinit var edtEmail:EditText
+    private lateinit var edtPassword:EditText
+    private lateinit var btnLogin:Button
+    private lateinit var btnSignup:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
