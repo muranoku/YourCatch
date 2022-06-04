676d06b5d9ee7692ea9ed4e6dd747934b8afea8f
garagara <ayuyeaun@gmail.com>
2022-06-05 00:54:59 +0900

    add:画面遷移

--- app/src/main/java/com/example/yourcatch/SignUp.kt
+++ app/src/main/java/com/example/yourcatch/SignUp.kt
@@ -1,9 +1,11 @@
 package com.example.yourcatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 
 class SignUp : AppCompatActivity() {
@@ -33,6 +35,13 @@ class SignUp : AppCompatActivity() {
     }
 
     private fun signUp(email: String, password: String){
-
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@SignUp,MainActivity3::class.java )
+                } else {
+                    Toast.makeText(this@SignUp, "Some error occurred",Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
